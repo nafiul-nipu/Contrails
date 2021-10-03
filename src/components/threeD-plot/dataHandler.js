4f5944597db32c3e4d19663a5a3b143cda7d86cc
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-10-03 17:10:25 -0500

    another added

--- src/components/threeD-plot/dataHandler.js
+++ src/components/threeD-plot/dataHandler.js
@@ -16,7 +16,7 @@ const loader = (folder, file, filter) =>{
         let url = `https://raw.githubusercontent.com/CarlaFloricel/Contrails/master/src/data/volume_data/${folder}/${file}.csv`;
         let dataBuffer = []
         let positions = []
-        // console.log(url)
+        console.log(url)
         d3.csv(url, data => {
                     
             // console.log(url)
