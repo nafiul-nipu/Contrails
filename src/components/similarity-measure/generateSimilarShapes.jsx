84beee3c4b73a907dc6d522aa291e9417555aa00
nafiulnipu <mnipu2@uic.edu>
2022-02-28 15:43:47 -0600

    shape data and it's componenet added

---
+++ src/components/similarity-measure/generateSimilarShapes.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { Col, Row } from 'react-bootstrap';
+
+// import * as d3 from 'd3'
+import DrawShapes from "./DrawShapes.component";
+
+import neighbors from '../data-component/similarData/neighbors.json'
+import neighbors2 from '../data-component/similarData/neighbors2.json'
+
+class GenerateSimilarShapes extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+        this.neighbors = neighbors
+    }
+
+    componentDidMount(){
+    }
+
+    render(){
+        // console.log(this.props.dimension)
+        if(this.props.similarity === 'io'){
+            this.neighbors = neighbors2
+            // console.log(this.neighbors)
+        }else if(this.props.similarity === 'shape'){
+            this.neighbors = neighbors
+            // console.log(this.neighbors)
+        }
+        return(
+            <>
+            <Row xs={12} style={{height: '40vh'}}>
+                {
+                    this.neighbors[this.props.value].map((el, i) => {
+                        // console.log(el, i)
+                        // let ft = el.split('_')
+                        // let folder = ft[0]
+                        // let time = ft[1]
+                        if(i < 3){
+                            return(
+                                <Col id='shape' xs={4} style={{height: '40vh'}} key={i}>
+                                <DrawShapes
+                                    key = {`${el}`}
+                                    folder ={this.props.folder}     
+                                    time={el}    
+                                    id={i}                      
+                                />                            
+                                </Col>
+                            )                                    
+
+                        }
+                    })
+                } 
+
+                </Row>
+
+                <Row xs={12} style={{height: '40vh'}}>
+                {
+                    this.neighbors[this.props.value].map((el, i) => {
+                        // console.log(el, i)
+                        if(i > 2){
+                            return(
+                                <Col id='shape' xs={4} style={{height: '40vh'}} key={i}>
+                                <DrawShapes
+                                    key = {`${this.props.value}-${el}`}
+                                    folder ={this.props.folder}     
+                                    time={el}      
+                                    id={i}                    
+                                />                            
+                                </Col>
+                            ) 
+                        }
+                    })
+                } 
+
+                </Row>
+            </>
+        )
+
+    }
+}
+
+export default GenerateSimilarShapes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/similarity-measure/shapeContainer.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+// import * as d3 from 'd3'
+import './shapeContainer.css'
+
+import Container from 'react-bootstrap/Container'
+import { Col, Row, Form} from 'react-bootstrap';
+
+import GenerateSimilarShapes from '../scatter/generateSimilarShapes';
+
+
+
+
+class ShapeContainerComponent extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            value: '210', 
+            similarity: 'shape', 
+            folder: 'newData'
+        }
+        // this.dataTime = {
+        //     contrails1: [0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2],
+        //     contrails2: [0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2],
+        //     contrails3: [0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2],
+        //     newData:[210,211,212,213,214,215,216,217,218,219]
+        // }
+        this.simulation = [210,211,212,213,214,215,216,217,218,219]
+        this.handleOnChange = this.handleOnChange.bind(this)
+        this.handleRadioChange = this.handleRadioChange.bind(this)
+    }
+
+    componentDidMount(){
+        // let up = parseInt(this.divs / 2)
+        // this.setState({
+        //     upper: up,
+        //     lower: parseInt(this.divs - up)
+        // })
+
+    }
+    handleOnChange(){
+        var val = document.getElementById("selection").value;
+        this.setState({value: val});
+        // console.log(val)
+
+    }
+
+    handleRadioChange(event){
+        // console.log(event.target.value)
+        this.setState({similarity: event.target.value})
+    }
+
+
+
+    render(){
+        // console.log(this.state)
+        if(this.state.value){
+            console.log('container')
+            console.log(this.state)
+            return(
+                <Container fluid>
+                    <Row xs={12} style={{height: '5vh'}}>
+                    <Col xs={4}>
+                    <Form>
+                    <Form.Group >
+                        <Form.Label id='space'>Simiarity based on : </Form.Label>
+                        <Form.Check
+                        inline
+                        label="Shape"
+                        name="TGroup"
+                        type="radio"
+                        id={`inline-radio-3`}
+                        value="shape"
+                        className="radin"
+                        defaultChecked
+                        onChange={this.handleRadioChange}
+                        />
+
+                        <Form.Check
+                        inline
+                        label="I/O"
+                        name="TGroup"
+                        type="radio"
+                        id={`inline-radio-4`}
+                        value="io"
+                        onChange={this.handleRadioChange}
+                        
+                        />
+                    </Form.Group>
+                    </Form>
+                        </Col>
+                        <Col xs={2}>
+                            <select id='selection' onChange={this.handleOnChange}>
+                                {/* <option value='contrails1'>contrails1</option> */}
+                                {
+                                    this.simulation.map((el, i) =>{
+                                        return <option value={`${el}`} key={i}>{el}</option>
+                                    })
+                                }
+                            </select>
+                        </Col>
+                    </Row>
+
+                    <GenerateSimilarShapes 
+                        value = {this.state.value}
+                        folder = {this.state.folder}
+                        similarity = {this.state.similarity}
+                    />
+
+                </Container>
+            )
+            
+        }
+        
+    }
+}
+
+export default ShapeContainerComponent ;
