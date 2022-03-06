90db85ac7153348be5f8170c47e1ee9416b93b1b
nafiulnipu <mnipu2@uic.edu>
2022-03-06 13:00:46 -0600

    tooltip bug fixed

--- src/components/contrailsEvolution/NodeDiagram.js
+++ src/components/contrailsEvolution/NodeDiagram.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3'
-import { Tooltip, Overlay } from 'react-bootstrap'
+import { Tooltip, Overlay, OverlayTrigger } from 'react-bootstrap'
 import { useState, useRef } from 'react';
 
 
@@ -8,48 +8,49 @@ export const NodeDiagram = ({nodes,name}) => {
   // const target = useRef(null);
 
   return nodes.map(node=>{
-    const [show, setShow] = useState(false);
-    const target = useRef(null);
+    // const [show, setShow] = useState(false);
+    // const target = useRef(null);
     return(
-    <>    
-    <circle className="circleCon"
-      ref={target}
-      key={node.id}
-      cx={node.x}
-      cy={node.y}
-      r={node.r}
-      onMouseEnter={() => {
-        // console.log('hovered')
-        setShow(true)
-        d3.selectAll(`.${name}${node.id}`)
-          .style('stroke-opacity', 1)
-          .style('stroke', '#FF6F61')
-
-
-      }}
-      onMouseOut={() => {
-        // console.log("hover Out")
-        setShow(false)
-        d3.selectAll('#pathCon')
-          .style('stroke-opacity', 0.5)
-          .style('stroke', 'black')
-      }}
-    />
-
-    <Overlay target={target.current} show={show} placement="left">
-        {(props) => (
-          <Tooltip id="overlay-example" {...props}>
+    <>   
+      <OverlayTrigger
+        key={node.id}
+        placement={'left'}
+        overlay={
+          <Tooltip id={`tooltip-left`}>
             <p>{`ID : ${node.id}`} <br/>
-              {`Total Particles : ${node.particles}`} <br/>
-              {`Total Mass: ${node.mass}`} <br/>
-              {`Avg. Temp: ${node.temp}`} <br/>
-              {`Total Length: ${node.length}`}
-            </p>
-             
+                {`Total Particles : ${node.particles}`} <br/>
+                {`Total Mass: ${node.mass}`} <br/>
+                {`Avg. Temp: ${node.temp}`} <br/>
+                {`Total Length: ${node.length}`}
+              </p>
           </Tooltip>
-        )}
-      </Overlay>
+        }
+      > 
+      <circle className="circleCon"
+        // ref={target}
+        key={node.id}
+        cx={node.x}
+        cy={node.y}
+        r={node.r}
+        onMouseEnter={() => {
+          console.log('hovered')
+          // setShow(true)
+          d3.selectAll(`.${name}${node.id}`)
+            .style('stroke-opacity', 1)
+            .style('stroke', '#FF6F61')
 
+
+        }}
+        onMouseLeave={() => {
+          console.log("hover Out")
+          // setShow(false)
+          d3.selectAll('#pathCon')
+            .style('stroke-opacity', 0.5)
+            .style('stroke', 'black')
+        }}
+      />
+
+      </OverlayTrigger>
     </>
  
 )})
