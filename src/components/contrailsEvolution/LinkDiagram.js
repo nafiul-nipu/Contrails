1869802b1de2be0804e752965a86ebdfcdcb2d2b
nafiulnipu <mnipu2@uic.edu>
2023-03-01 17:04:01 -0600

    stroke width updated

--- src/components/contrailsEvolution/LinkDiagram.js
+++ src/components/contrailsEvolution/LinkDiagram.js
@@ -1,16 +1,17 @@
-import { linkHorizontal} from "d3";
+import { linkHorizontal } from "d3";
 
 
-export const LinkDiagram = ({links, name}) => {
-  // console.log(name)
+export const LinkDiagram = ({ links, name }) => {
+  console.log(links)
   return links.map(link => (
     <path id="pathCon"
       key={`${name}${link.id1} ${name}${link.id2}`}
       className={`${name}${link.id1} ${name}${link.id2}`}
       d={linkHorizontal()
-          .source(d =>d.source)
-          .target(d=>d.target)
-          (link)
-        }
+        .source(d => d.source)
+        .target(d => d.target)
+        (link)
+      }
     />
-  ))};
+  ))
+};
